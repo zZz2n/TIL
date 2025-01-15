ee9d9320ae898b2c04d05ab7ac8f0b7c5b65b755
zZz2n <sy100409@naver.com>
2025-01-15 11:20:17 +0900

    20250115

--- js practice/09_callbackfun.js
+++ js practice/09_callbackfun.js
@@ -1,7 +1,25 @@
-const operation = function(func, num) {
-  return func(num);
+const multiplier = function(n){
+  return (x) => x * n;
 };
 
-let double = (x) => x * 2;
+const double = multiplier(2);
+const triple = multiplier(3);
 
-console.log(operation(double, 3));
+console.log(double(5));
+console.log(triple(5));
+
+const operation = {
+  add : (a, b) => a+b,
+  multiply : (a,b) => a*b
+};
+
+console.log(operation.add(3,4))
+console.log(operation.multiply(4,5))
+
+// const operation = function(func, num) {
+//   return func(num);
+// };
+
+// let double = (x) => x * 2;
+
+// console.log(operation(double, 3));
